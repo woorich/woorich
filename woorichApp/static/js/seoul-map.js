eb8423566233f8887da7620851a3108a440e3b0d
WooYoon Lee <45444757+eeoooo@users.noreply.github.com>
2023-08-29 16:46:54 +0900

    FEAT: render seoul-si map😥❣️

--- woorichApp/static/js/seoul-map.js
+++ woorichApp/static/js/seoul-map.js
@@ -1,19 +1,48 @@
-mapboxgl.accessToken = 'pk.eyJ1Ijoid29veW9vbndpbm5pZSIsImEiOiJjbGx1ZWl2ZjkxOWt0M2RtbGY3MWl5N29jIn0.Ksbm1ZDp2VptXVlOldqguw';
+mapboxgl.accessToken = 'pk.eyJ1Ijoid29veW9vbndpbm5pZSIsImEiOiJjbGx1anJkdWsxY29kM2Zuem1naWpqdWc2In0.SAvOfExJLVtezNxgW8GAig';
 const map = new mapboxgl.Map({
     container: 'map-container', // container ID
     center: [126.9779692, 37.566535], // starting position [lng, lat]
-    zoom: 11, // starting zoom
+    zoom: 10, // starting zoom
     style: 'mapbox://styles/mapbox/light-v10', // style URL or style object
+    
     hash: true, // sync `center`, `zoom`, `pitch`, and `bearing` with URL
     // Use `transformRequest` to modify requests that begin with `http://myHost`.
     language: "auto",
     transformRequest: (url, resourceType) => {
-            if (resourceType === 'Source' && url.startsWith('http://myHost')) {
-                return {
-                    url: url.replace('http', 'https'),
-                    headers: {'my-custom-header': true},
-                    credentials: 'include'  // Include cookies for cross-origin requests
-                };
-            }
+        if (resourceType === 'Source' && url.startsWith('http://myHost')) {
+            return {
+                url: url.replace('http', 'https'),
+                headers: {'my-custom-header': true},
+                credentials: 'include'  // Include cookies for cross-origin requests
+            };
+        }
+    }
+});
+map.on('load', function() {
+    
+    map.addSource('tileset_data', {
+        "url": "mapbox://wooyoonwinnie.aahbjlxw",
+        "type": "vector"
+    });
+    map.addLayer({
+        'id': 'background',
+        'type': 'background',
+        'source': 'tileset_data',
+        'source-layer': 'seoul_gu-3hhcst',
+        'paint' : {
+            'background-color': 'hsla(0, 0%, 100%, 0.7)'
         }
     });
+    map.addLayer({
+        'id': 'fill',
+        'type': 'fill',
+        'source': 'tileset_data',
+        'source-layer': 'seoul_gu-3hhcst',
+        'paint': {
+            'fill-color': 'hsla(208, 66%, 46%, 0.45)',
+            'fill-opacity': 0.75,
+            'fill-outline-color': 'hsl(0, 100%, 100%)'
+        }
+    });
+
+});
