d8fe437e41344e246aaee8481eb7ea76f707bbc0
WooYoon Lee <45444757+eeoooo@users.noreply.github.com>
2023-08-30 12:04:30 +0900

    FEAT: make select menu to show seoul geo info

--- woorichApp/static/js/seoul-map.js
+++ woorichApp/static/js/seoul-map.js
@@ -48,9 +48,9 @@ map.on('load', function() {
 });
 
 function show_report() {
-    var selectedGu = document.querySelector('.seoul-gu-selected option:checked').textContent;
+    var selectedGu = document.getElementById('dynamic-select-gu').value;
 
-    var selectedDong = document.querySelector('.seoul-dong-selected option:checked').textContent;
+    var selectedDong = document.getElementById('dynamic-select-dong').value;
 
     // Display the selected texts or perform any other action
     console.log("Selected 행정구: " + selectedGu + "\nSelected 행정동: " + selectedDong);
@@ -58,3 +58,67 @@ function show_report() {
     var resultDiv = document.getElementById('result'); // Replace 'result' with the actual ID of your <div>
     resultDiv.innerHTML = "<br> 선택된 행정구: " + "<h3>" + selectedGu + "</h3>" + "<br> 선택된 행정동: " + "<h3>" + selectedDong+"</h3><br>";
 }
+
+////////////////////////////////////////////////////////////////////////////////
+
+const selectElementGu = document.getElementById('dynamic-select-gu');
+const selectElementDong = document.getElementById('dynamic-select-dong');
+
+const staticUrl = document.currentScript.getAttribute('staticUrl');
+// Replace 'administrative_dongs.csv' with the path to your CSV file
+fetch(staticUrl + 'gu-dong-data.csv', {
+    headers: {
+        'Content-Type': 'text/csv; charset=cp-949' // Specify the character encoding
+    }
+})
+.then(response => response.text()) // Read the CSV file as text
+.then(csvData => {
+    // Parse the CSV data into an array of objects
+    const csvArray = csvData.split('\n').map(row => row.split(','));
+
+    // Remove the header row if present (optional)
+    const header = csvArray[0];
+    if (header.length > 1) {
+        csvArray.shift();
+    }
+
+    const guSet = new Set(); // Use a Set to store unique 행정구 values
+
+    // Loop through the data to populate 행정구 dropdown and collect unique values
+    csvArray.forEach(row => {
+        const guValue = row[2]; // Assuming 행정구 is in column 3 (index 2)
+        if (guValue) {
+            guSet.add(guValue); // Add 행정구 value to the Set if it's not undefined
+        }
+    });
+
+    // Populate the 행정구 dropdown with unique values
+    guSet.forEach(guValue => {
+        const option = document.createElement('option');
+        option.value = guValue;
+        option.text = guValue;
+        selectElementGu.appendChild(option);
+    });
+
+    // Add an event listener to 행정구 dropdown to dynamically populate 행정동 dropdown
+    selectElementGu.addEventListener('change', () => {
+        // Clear existing options in 행정동 dropdown
+        selectElementDong.innerHTML = '';
+
+        const selectedGu = selectElementGu.value;
+
+        // Loop through the data to find matching 행정동 values for the selected 행정구
+        csvArray.forEach(row => {
+            const guValue = row[2]; // Assuming 행정구 is in column 3 (index 2)
+            const dongValue = row[3]; // Assuming 행정동 is in column 4 (index 3)
+
+            if (guValue === selectedGu) {
+                const option = document.createElement('option');
+                option.value = dongValue;
+                option.text = dongValue;
+                selectElementDong.appendChild(option);
+            }
+        });
+    });
+})
+.catch(error => console.error('Error:', error));
